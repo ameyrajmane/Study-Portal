f67f1cd68b7bb008b42346ea254a532df1b73ab3
ameyrajmane <ameyrajmane8@gmail.com>
2022-03-17 15:28:05 +0530

    youtube.html added

--- dashboard/views.py
+++ dashboard/views.py
@@ -33,6 +33,9 @@ class NotesDetailView(generic.DetailView):
     model = Notes
 
     
+    
+    
+    
 def homework(request):
     if request.method == 'POST':
         form = HomeworkForm(request.POST)
@@ -45,9 +48,19 @@ def homework(request):
                     finished = False
             except:
                 finished = False
-    
-    
-    form = HomeworkForm()
+            homeworks = Homework(
+                user = request.user,
+                subject = request.POST['subject'],
+                title = request.POST['title'],
+                description = request.POST['description'],
+                due = request.POST['due'],
+                is_finished = finished
+            )
+            homeworks.save()
+            messages.success(request, f'Homework added from {request.user.username}!!')
+            
+    else:
+            form = HomeworkForm()
     homework = Homework.objects.filter(user=request.user)
     
     if len(homework) == 0:
@@ -57,3 +70,23 @@ def homework(request):
     
     context = {'homeworks':homework, 'homeworks_done':homework_done, 'form':form}
     return render(request, 'dashboard/homework.html', context)
+
+
+def update_homework(request, pk=None):
+    homework = Homework.objects.get(id=pk)
+    if homework.is_finished == True:
+        homework.is_finished = False
+    else:
+        homework.is_finished = True
+    homework.save()
+    return redirect('homework')
+
+
+
+def delete_homework(request,pk=None):
+    Homework.objects.get(id=pk).delete()
+    return redirect('homework')
+
+
+def youtube(request):
+    return render(request, 'dashboard/youtube.html')
