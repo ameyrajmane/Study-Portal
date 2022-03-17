75b6b3a86be48a59bc73d7b8d0919364ef698189
ameyrajmane <ameyrajmane8@gmail.com>
2022-03-17 18:11:09 +0530

    todo section done

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -5,3 +5,4 @@ from django.contrib import admin
 # Register your models here.
 admin.site.register(Notes)
 admin.site.register(Homework)
+admin.site.register(Todo)
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from matplotlib.backend_bases import MouseEvent
 from matplotlib.pyplot import cla
 # Create your models here.
 
@@ -22,6 +23,15 @@ class Homework(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     due = models.DateTimeField()
+    is_finished = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.title
+    
+    
+class Todo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
     is_finished = models.BooleanField(default=False)
     
     def __str__(self):
